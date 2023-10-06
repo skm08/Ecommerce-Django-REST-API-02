fced5a20f0a100a2f1843018a631db03263c91f1
skm08 <dweep.plus@gmail.com>
2023-10-06 16:20:53 +0600

    Initial Commit

---
+++ product/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title=models.CharField(max_length=256,unique=True)
+    slug=models.SlugField(unique=True)
+    featured=models.BooleanField(default=False)
+    created_date=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering=['title']
+
+    def __str__(self) -> str:
+        return self.title
+    
+class Product(models.Model):
+    category=models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
+    title=models.CharField(max_length=256,unique=True)
+    slug=models.SlugField(unique=True)
+    featured=models.BooleanField(default=False)
+    thumbnail=models.URLField()
+    price=models.DecimalField(max_digits=8, decimal_places=2)
+    description=models.TextField(null=True,blank=True)
+    created_date=models.DateTimeField(auto_now_add=True)
+    updated_date=models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
